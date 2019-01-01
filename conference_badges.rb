d3b465631770c8dbd124f91397a3a3f389e7fd87
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-01-01 20:04:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_array)
+  new_array = []
+  name_array.each do |sub_array|
+    new_array.push("Hello, my name is #{sub_array}.")
+  end
+  return new_array
+end
+
+def assign_rooms(names)
+  
+end
+
+def printer
+  
+end
