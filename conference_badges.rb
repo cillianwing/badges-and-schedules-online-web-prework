563a12bae44c7dca9e9a551856b89a5625868ebd
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-01-01 21:13:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -25,5 +25,7 @@ def printer(attendees)
   batch_badge_creator(attendees).each do |sub_array|
     puts sub_array
   end
-  
+  assign_rooms(attendees).each do |sub_array|
+    puts sub_array
+  end
 end
